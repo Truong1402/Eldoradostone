e18df8b4b2e86d7e5fcb3375a837b923abf16990
Truong1402 <114339668+Truong1402@users.noreply.github.com>
2023-06-20 10:20:44 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         speed: 500,
         autoplay: true,
         arrows: false,
-         fade: true,
+        fade: true,
         draggable: false,
         cssEase: 'linear'
 
